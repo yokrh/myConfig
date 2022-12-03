79155370bb06d8e34f8abb4439058ae100dd036d
yokrh <y.oka.gml@gmail.com>
2022-12-03 11:15:05 +0900

    add hammerspoon-shiftit config

---
+++ init.lua
@@ -0,0 +1,10 @@
+hs.loadSpoon("ShiftIt")
+spoon.ShiftIt:bindHotkeys({
+  left = { { 'option', 'cmd' }, 'left' },
+  down = { { 'option', 'cmd' }, 'down' },
+  up = { { 'option', 'cmd' }, 'up' },
+  right = { { 'option', 'cmd' }, 'right' },
+  maximum = { { 'option', 'cmd' }, 'f' },
+})
+spoon.ShiftIt:setWindowCyclingSizes({ 50, 33, 67 }, { 50 })
+
